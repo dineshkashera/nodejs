d4fb8116e330aa7d0f09a5b1c643084d7bd98ded
dineshkashera <dineshkashera@gmail.com>
2021-12-18 19:55:19 +0530

    check email and url using validator npm module

--- NODE-COURSE/node-apps/app.js
+++ NODE-COURSE/node-apps/app.js
@@ -3,3 +3,8 @@ fs.writeFileSync('notes.txt','hello, i am dineshkashera');
 
 const getnotes = require('./module-exports');
 console.log(getnotes());
+
+const validator = require('validator');
+console.log(validator.isEmail('dineshkashera@gmail.com'));//check is email
+
+console.log(validator.isURL('https:google.com'));
