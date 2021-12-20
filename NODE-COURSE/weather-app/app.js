c6bbe77fa63b429e64cc0b0dfcd807341c0fa582
dineshkashera <dineshkashera@gmail.com>
2021-12-21 00:15:35 +0530

    get current temprature by enter address in progress

--- NODE-COURSE/weather-app/app.js
+++ NODE-COURSE/weather-app/app.js
@@ -1,7 +1,51 @@
-const request   =   require('request');
-const apiURL    =   'http://api.weatherstack.com/current?access_key=dc65e012d473c1e07f20ee3a2bb53561&query=37.8267,-122.4233';
+const request           =    require('request');
+const yargs             =    require('yargs');
+const stackapiURL       =    'http://api.weatherstack.com/current?access_key=dc65e012d473c1e07f20ee3a2bb53561';
+const stackQuery        =    '&query=';
+const metaboxURL        =    'https://api.mapbox.com/geocoding/v5/mapbox.places/';
+const mapboxAPI         =    'pk.eyJ1IjoiZGluZXNoa2FzaGVyYSIsImEiOiJja3hleGttOWcxNmc5MnVtbTl3ajV6bm16In0.fxwuVxBZNcI630kRrxcYmw';
+const metaquery         =    '?access_token='+mapboxAPI;
 
-request({url:apiURL},(error, response) => {
-        const data = JSON.parse(response.body);
-        console.log(data.current);
+//create command to get temprature by place name
+yargs.command({
+        command:'temp',
+        describe:'Get latest Temprature',
+        builder:{
+                location:{
+                        describe:'Enter current location to get temprature',
+                        demandOption: true,
+                        type:'string'
+                }
+        },
+        handler(argv){
+                getCurrentTemprature(argv.location);
+        }
 });
+
+const getCurrentTemprature = (location) => {
+        const requestURI = metaboxURL+location+'.json'+metaquery;
+        request({url:requestURI,json:true},(error, response) => {
+            if(error){
+                 console.log(error);
+            }else {
+                    const getLatLang = response.body;
+                    if(getLatLang.features.length > 0){
+                        const latlong = getLatLang.features['0'].center;
+                        const coordinate = latlong.reverse().toString();
+                        console.log(coordinate);
+                    }else{
+                        console.log('Incorrect location enter!');
+                    }
+
+            }
+        });
+}
+
+yargs.parse();
+/*request({url:stackapiURL,json:true},(error, response) => {
+        const weather = response.body.current;
+        const temperature = weather.temperature;
+        const precip      = weather.precip;
+        const msg         = 'It is currently '+temperature+' degree, there is '+precip+'% chance of rain';
+        console.log(msg);
+});*/
