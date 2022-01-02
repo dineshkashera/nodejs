7b991932b9ffe5804cce3d6bb909d2f46a483d26
dineshkashera <dineshkashera@gmail.com>
2022-01-02 14:36:50 +0530

    added template engine handle bar and make pages dynamic by using hbs

--- NODE-COURSE/web-server-express/src/app.js
+++ NODE-COURSE/web-server-express/src/app.js
@@ -1,11 +1,25 @@
-const express = require('express');
-const path  = require('path');
-const app = express();
+const express   = require('express');
+const path      = require('path');
+const app       = express();
 
 const publicDirPath = path.join(__dirname,'../public');
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirPath));
+app.get('',(req, res) => {
+  res.render('index',{
+      title:"Start",
+      link:"About Us"
+  });
+});
 
-app.get('',(req,res) => {
+app.get('/about',(req, res) => {
+    res.render('about',{
+        title:"About us",
+        link:"Home"
+    });
+});
+
+/*app.get('',(req,res) => {
     res.send('hello world');
 });
 
@@ -15,7 +29,7 @@ app.get('/help',(req,res) => {
 
 app.get('/about-us',(req,res) => {
     res.send('About us page loaded');
-});
+});*/
 
 app.listen(4000,() => {
     console.log('server started successfully');
