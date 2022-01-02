23820a5407e92f3b47d847c01062bcae224fe31c
dineshkashera <dineshkashera@gmail.com>
2022-01-02 15:40:05 +0530

    added partial templates by hbs.registerPartials(hbsPartialPath);

--- NODE-COURSE/web-server-express/src/app.js
+++ NODE-COURSE/web-server-express/src/app.js
@@ -1,21 +1,34 @@
 const express   = require('express');
 const path      = require('path');
 const app       = express();
+const hbs       = require('hbs');
 
+//defining path for express
 const publicDirPath = path.join(__dirname,'../public');
+const hbsViewPath = path.join(__dirname,'../templates');
+const hbsPartialPath = path.join(__dirname,'../templates/partials');
+
+//setup handlebar for views and hbs
 app.set('view engine', 'hbs');
+app.set('views',hbsViewPath);
+hbs.registerPartials(hbsPartialPath);
+
+//setup static directory to serve
 app.use(express.static(publicDirPath));
+
 app.get('',(req, res) => {
   res.render('index',{
       title:"Start",
-      link:"About Us"
+      link:"About Us",
+      copywrite:"by dineshkashera"
   });
 });
 
 app.get('/about',(req, res) => {
     res.render('about',{
         title:"About us",
-        link:"Home"
+        link:"Home",
+        copywrite:"by dineshkashera"
     });
 });
 
