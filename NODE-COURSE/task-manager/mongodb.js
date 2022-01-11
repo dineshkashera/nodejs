7454dbc59d4e4ae7634d09ed6a0d803ee03faa55
dineshkashera <dineshkashera@gmail.com>
2022-01-11 22:18:19 +0530

    Added:mutiple documents in collection in mongodb

--- NODE-COURSE/task-manager/mongodb.js
+++ NODE-COURSE/task-manager/mongodb.js
@@ -11,8 +11,34 @@ mongoClient.connect(connectionUrl,{useNewUrlParser:true},(error,client) => {
     }
     console.log('connected successfully');
     const db = client.db(dbName);
-    db.collection('user').insertOne({
+    /*db.collection('user').insertOne({
         'name':'dineshkashera',
         'role':'software developer'
+    });*/
+    const manydocument = [{
+        "description":"desc 1",
+        "completed":true
+    },{
+        "description":"desc 2",
+        "completed":false
+    },{
+        "description":"desc 3",
+        "completed":true
+    }];
+    db.collection('tasks').insertMany([{
+        "description":"desc 1",
+        "completed":true
+    },{
+        "description":"desc 2",
+        "completed":false
+    },{
+        "description":"desc 3",
+        "completed":true
+    }],(error,result) => {
+        if(error){
+            return console.log(error);
+        }
+
+        console.log(result.ops);
     });
 });
