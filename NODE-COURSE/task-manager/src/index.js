06e6c696e8c7d6b4521288988709c36fed427968
dineshkashera <dineshkashera@gmail.com>
2022-01-21 00:33:44 +0530

    Created task api

--- NODE-COURSE/task-manager/src/index.js
+++ NODE-COURSE/task-manager/src/index.js
@@ -6,15 +6,27 @@ app.use(express.json());//automatically parse request json
 require('./db/mongoose');// initiate db connection
 
 const User = require('./model/user');//get user model
+const Task = require('./model/task');
 
+//user route creation
 app.post('/user',(req,res) => {
 
     const user = new User(req.body);
 
     user.save().then(() => {
-        res.status('201').send({'success':true,'data':User(req.body)});
+        res.status('201').send({'success':true,'data':'User created'});
     }).catch((error) => {
-        res.status('400').send({'success':false,'data':error});
+        res.status('400').send({'success':false,'data':error.message});
+    });
+});
+
+//Task route creation
+app.post('/task',(req, res)=>{
+    const task = new Task(req.body);
+    task.save().then(() => {
+        res.status('201').send({'success':true,"data":'Task Created','task-obj':task});
+    }).catch((error) => {
+        res.status('400').send({'success':false,'data':error.message});
     });
 });
 
