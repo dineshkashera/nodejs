d64315f01669ff897f4b21ffd5dc7acc0998a68a
dineshkashera <dineshkashera@gmail.com>
2022-01-22 00:31:52 +0530

    get users/user and tasks/task

--- NODE-COURSE/task-manager/src/index.js
+++ NODE-COURSE/task-manager/src/index.js
@@ -30,6 +30,54 @@ app.post('/task',(req, res)=>{
     });
 });
 
+//get all users
+app.get('/users',(req,res) => {
+
+    User.find({}).then((getusers)=>{
+        res.status('200').send({'status':true,'data':getusers})
+    }).catch((e) => {
+        res.status('400').send({'success':false,'data':error.message})
+    });
+});
+
+//get single user by id
+app.get('/user/:id',(req,res) => {
+
+    const _id = req.params.id;
+    User.findById(_id).then((singleuser)=>{
+        if(!singleuser){
+            res.status('200').send({'status':true,'data':'no user found'})
+        }
+        res.status('200').send({'status':true,'data':singleuser})
+    }).catch((e) => {
+        res.status('400').send({'success':false,'data':error.message})
+    });
+});
+
+//get all task
+app.get('/tasks',(req,res) => {
+
+    Task.find({}).then((getTasks)=>{
+        res.status('200').send({'status':true,'data':getTasks})
+    }).catch((e) => {
+        res.status('400').send({'success':false,'data':error.message})
+    });
+});
+
+//get single id
+app.get('/task/:id',(req,res) => {
+
+    const _id = req.params.id;
+    Task.findById(_id).then((singleTask)=>{
+        if(!singleTask){
+            res.status('200').send({'status':true,'data':'no Task found'})
+        }
+        res.status('200').send({'status':true,'data':singleTask})
+    }).catch((e) => {
+        res.status('400').send({'success':false,'data':error.message})
+    });
+});
+
 app.listen(port,() => {
    console.log('Server started on port::'+ port);
 });
