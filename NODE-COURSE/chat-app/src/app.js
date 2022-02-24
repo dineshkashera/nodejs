54cc79b8336bcdacc0d474e0fce6da390e1f5176
dineshkashera <dineshkashera@gmail.com>
2022-02-24 09:11:41 +0530

    Setup static page using express

--- NODE-COURSE/chat-app/src/app.js
+++ NODE-COURSE/chat-app/src/app.js
@@ -10,7 +10,7 @@ const port                  =   process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname,'../public');
 const hbsViewPath = path.join(__dirname,'../templates');
 const hbsPartialPath = path.join(__dirname,'../templates/partials');
-
+const router = require('./routers/routes');
 //setup handlebar for views and hbs
 app.set('view engine', 'hbs');
 app.set('views',hbsViewPath);
@@ -18,84 +18,7 @@ hbs.registerPartials(hbsPartialPath);
 
 //setup static directory to serve
 app.use(express.static(publicDirPath));
-
-app.get('',(req, res) => {
-    res.render('index',{
-        title:"Weather",
-        link:"About Us",
-        copywrite:"dineshkashera",
-        class:"index"
-    });
-});
-
-app.get('/about',(req, res) => {
-    res.render('about',{
-        title:"About us",
-        link:"Home",
-        copywrite:"dineshkashera",
-        class:"about-us"
-    });
-});
-
-app.get('/weather',(req,res) => {
-    //if(req.query.address){
-    forecast.getCurrentTemprature(req.query.address,({latlong ,location}) => {
-
-        const reqtempURI = stackapiURL+stackQuery+latlong;
-        request({url:reqtempURI,json:true},(error, {body}) => {
-            if(error){
-                res.send({
-                    error:"INCORRECT INPUT GIVEN"
-                });
-            }
-
-            const weather     = body.current;
-            const {temperature,precip} = weather;
-            const msg         = 'It is currently '+temperature+' degree, there is '+precip+'% chance of rain';
-            res.send({
-                address:req.query.address,
-                location:location,
-                forecast:msg
-            });
-        });
-    });
-    // }else{
-    //   res.send({
-    //     error:"No address given"
-    //});
-    //}
-});
-/*app.get('',(req,res) => {
-    res.send('hello world');
-});
-
-app.get('/help',(req,res) => {
-    res.send('Help page loaded');
-});
-
-app.get('/about-us',(req,res) => {
-    res.send('About us page loaded');
-});*/
-
-app.get('/about/*',(req,res) => {
-    res.render('404',{
-        title:"404 Error page",
-        link:"Home",
-        errorMessage:"Url not found",
-        copywrite:"dineshkashera",
-        class:"about-us-404"
-    });
-});
-
-app.get('*',(req,res) => {
-    res.render('404',{
-        title:"404 Error page",
-        link:"Home",
-        errorMessage:"page not found",
-        copywrite:"dineshkashera",
-        class:"error-404"
-    });
-});
+app.use(router);
 
 app.listen(port,() => {
     console.log('server started successfully on port::' + port);
--- NODE-COURSE/chat-app/src/routers/routes.js
+++ NODE-COURSE/chat-app/src/routers/routes.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const router = new express.Router();
+
+router.get('',async (req,res) => {
+    res.render('index',{
+        title:"Chat app",
+        link:"Chat Panel",
+        copywrite:"dineshkashera",
+        class:"chat-panel",
+        intro_heading:'Chat Panel',
+        intro_desc:'This is online chat panel, enjoy it!'
+    });
+});
+
+module.exports = router;
