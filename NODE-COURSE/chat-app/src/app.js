63516bc7d6705bc56ee4988b9a5d9d59d4c8a4eb
dineshkashera <dineshkashera@gmail.com>
2022-02-25 11:13:26 +0530

    setup websocket io server

--- NODE-COURSE/chat-app/src/app.js
+++ NODE-COURSE/chat-app/src/app.js
@@ -3,8 +3,13 @@ const path                  =   require('path');
 const app                   =   express();
 const hbs                   =   require('hbs');
 const request               =   require('request');
+const http                  =   require('http');
+const socketio              =   require('socket.io');
 
 const port                  =   process.env.PORT || 3000;
+
+const server                =   http.createServer(app);//create server
+const io                    =   socketio(server);
 
 //defining path for express
 const publicDirPath = path.join(__dirname,'../public');
@@ -20,6 +25,10 @@ hbs.registerPartials(hbsPartialPath);
 app.use(express.static(publicDirPath));
 app.use(router);
 
-app.listen(port,() => {
+io.on('connection',(port) => {
+ console.log('New connection on');
+});
+
+server.listen(port,() => {
     console.log('server started successfully on port::' + port);
 });
